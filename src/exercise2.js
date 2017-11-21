36aac28ad13d3b68863c99660f9b9cc73e883ed7
dany corbineau <danycor2@hotmail.fr>
2017-11-21 16:01:01 +0100

    ex 1,2 fini

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,9 @@
 exports.run = function(input) {
-  console.log("Exercise #2");
+    var bignumber=0;
+    for(var j=0;j<input.length;j++)
+    {
+        if(Number(input.charAt(j))>bignumber)
+          bignumber=Number(input.charAt(j));
+    }
+    return bignumber;
 };
