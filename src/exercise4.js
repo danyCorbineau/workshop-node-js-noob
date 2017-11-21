1bd1d7a1a757277250d5e3d674daf2e53d6abb72
dany corbineau <danycor2@hotmail.fr>
2017-11-21 16:11:20 +0100

    exercice 4 fini

--- src/exercise4.js
+++ src/exercise4.js
@@ -2,6 +2,13 @@ exports.run = function(input) {
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
+  var repeat=function (a,b)
+  {
+      var chaine=b;
+      for(var j=1;j<a;j++)
+        chaine+=`, ${b}`;
+      return chaine;
+  }
 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(3, "Beetlejuice", repeat);
