f351de9e61bb8ae9efa109225469f05e3af7be3e
dany corbineau <danycor2@hotmail.fr>
2017-11-21 16:04:33 +0100

    exercice 3 fini et fonctionne

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,13 @@
 exports.run = function(input) {
-  console.log("Exercise #3");
+   var number=0;
+   input.forEach(elem => {
+      if(elem.multiply)
+      {
+        if(number==0)
+          number=elem.value;
+        else
+          number*=elem.value;
+      }
+   });
+   return number;
 };
